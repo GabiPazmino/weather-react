88d6bdb5647f6954578f962c2078693a3e0731ac
mgpa <127637340+GabiPazmino@users.noreply.github.com>
2023-10-29 17:22:40 -0500

    add tittle and github/portfolio links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>hola</h1>
+      <h1>Weather app</h1>
+
+      <br />
+      <footer>
+        This project was created by{" "}
+        <a href="https://gabipazminoportfolio.netlify.app/" target="_blank">
+          Gabi Pazmiño
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/GabiPazmino/weather-react" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
