30fe942e6911801d66c113077a8b307e2f4e179c
mgpa <127637340+GabiPazmino@users.noreply.github.com>
2023-11-04 17:57:34 -0500

    fix css forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,15 +4,17 @@ import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
   return (
-    <div className="row">
-      <div className="col">
-        <div className="WeatherForecast-day">Fri</div>
-        <WeatherIcon code="01d" size={36} />
-        <br />
-        <br />
-        <div className="WeatherForecast-temperatures">
-          <span className="WeatherForecast-temperatures-max">19°C</span>{" "}
-          <span className="WeatherForecast-temperatures-min">10°C</span>
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Fri</div>
+          <WeatherIcon code="01d" size={36} />
+          <br />
+          <br />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperatures-max">19°C</span>{" "}
+            <span className="WeatherForecast-temperatures-min">10°C</span>
+          </div>
         </div>
       </div>
     </div>
